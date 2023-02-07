ae7ace4e79d9ed2b08cf78318df385b744eaf0cd
shake <shake@GWTN141-10>
2023-02-07 17:38:16 -0600

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,154 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmBurgers
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblHeading = New System.Windows.Forms.Label()
+        Me.btnPrime = New System.Windows.Forms.Button()
+        Me.btnSelectMeal = New System.Windows.Forms.Button()
+        Me.btnVeggie = New System.Windows.Forms.Button()
+        Me.lblInstructions = New System.Windows.Forms.Label()
+        Me.lblConfirmation = New System.Windows.Forms.Label()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.picVeggie = New System.Windows.Forms.PictureBox()
+        Me.picPrime = New System.Windows.Forms.PictureBox()
+        CType(Me.picVeggie, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picPrime, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'lblHeading
+        '
+        Me.lblHeading.Font = New System.Drawing.Font("Tahoma", 16.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblHeading.Location = New System.Drawing.Point(260, 9)
+        Me.lblHeading.MaximumSize = New System.Drawing.Size(305, 33)
+        Me.lblHeading.Name = "lblHeading"
+        Me.lblHeading.Size = New System.Drawing.Size(305, 33)
+        Me.lblHeading.TabIndex = 0
+        Me.lblHeading.Text = "Farm Burger Specials"
+        '
+        'btnPrime
+        '
+        Me.btnPrime.Location = New System.Drawing.Point(117, 347)
+        Me.btnPrime.Name = "btnPrime"
+        Me.btnPrime.Size = New System.Drawing.Size(128, 35)
+        Me.btnPrime.TabIndex = 3
+        Me.btnPrime.Text = "Prime Beef"
+        Me.btnPrime.UseVisualStyleBackColor = True
+        '
+        'btnSelectMeal
+        '
+        Me.btnSelectMeal.Location = New System.Drawing.Point(353, 347)
+        Me.btnSelectMeal.Name = "btnSelectMeal"
+        Me.btnSelectMeal.Size = New System.Drawing.Size(128, 35)
+        Me.btnSelectMeal.TabIndex = 4
+        Me.btnSelectMeal.Text = "Slelect Meal"
+        Me.btnSelectMeal.UseVisualStyleBackColor = True
+        '
+        'btnVeggie
+        '
+        Me.btnVeggie.Location = New System.Drawing.Point(584, 347)
+        Me.btnVeggie.Name = "btnVeggie"
+        Me.btnVeggie.Size = New System.Drawing.Size(128, 35)
+        Me.btnVeggie.TabIndex = 5
+        Me.btnVeggie.Text = "Veggie"
+        Me.btnVeggie.UseVisualStyleBackColor = True
+        '
+        'lblInstructions
+        '
+        Me.lblInstructions.Font = New System.Drawing.Font("Tahoma", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblInstructions.Location = New System.Drawing.Point(233, 397)
+        Me.lblInstructions.Name = "lblInstructions"
+        Me.lblInstructions.Size = New System.Drawing.Size(360, 18)
+        Me.lblInstructions.TabIndex = 6
+        Me.lblInstructions.Text = "Choose a burger and then click the Select Meal Button"
+        '
+        'lblConfirmation
+        '
+        Me.lblConfirmation.Font = New System.Drawing.Font("Tahoma", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblConfirmation.Location = New System.Drawing.Point(309, 447)
+        Me.lblConfirmation.Name = "lblConfirmation"
+        Me.lblConfirmation.Size = New System.Drawing.Size(172, 18)
+        Me.lblConfirmation.TabIndex = 7
+        Me.lblConfirmation.Text = "Enjoy your burger special"
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(353, 482)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(128, 35)
+        Me.btnExit.TabIndex = 8
+        Me.btnExit.Text = "Exit Window"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'picVeggie
+        '
+        Me.picVeggie.Image = Global.Weekly_Assignment_2.My.Resources.Resources.veggie
+        Me.picVeggie.Location = New System.Drawing.Point(539, 61)
+        Me.picVeggie.Name = "picVeggie"
+        Me.picVeggie.Size = New System.Drawing.Size(260, 250)
+        Me.picVeggie.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picVeggie.TabIndex = 2
+        Me.picVeggie.TabStop = False
+        '
+        'picPrime
+        '
+        Me.picPrime.ErrorImage = Nothing
+        Me.picPrime.Image = Global.Weekly_Assignment_2.My.Resources.Resources.prime
+        Me.picPrime.Location = New System.Drawing.Point(-5, 61)
+        Me.picPrime.Name = "picPrime"
+        Me.picPrime.Size = New System.Drawing.Size(260, 250)
+        Me.picPrime.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picPrime.TabIndex = 1
+        Me.picPrime.TabStop = False
+        '
+        'frmBurgers
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(784, 551)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.lblConfirmation)
+        Me.Controls.Add(Me.lblInstructions)
+        Me.Controls.Add(Me.btnVeggie)
+        Me.Controls.Add(Me.btnSelectMeal)
+        Me.Controls.Add(Me.btnPrime)
+        Me.Controls.Add(Me.picVeggie)
+        Me.Controls.Add(Me.picPrime)
+        Me.Controls.Add(Me.lblHeading)
+        Me.Name = "frmBurgers"
+        Me.Text = "Burger Specials"
+        CType(Me.picVeggie, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picPrime, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents lblHeading As Label
+    Friend WithEvents picPrime As PictureBox
+    Friend WithEvents picVeggie As PictureBox
+    Friend WithEvents btnPrime As Button
+    Friend WithEvents btnSelectMeal As Button
+    Friend WithEvents btnVeggie As Button
+    Friend WithEvents lblInstructions As Label
+    Friend WithEvents lblConfirmation As Label
+    Friend WithEvents btnExit As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class frmBurgers
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles lblConfirmation.Click
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+
+    End Sub
+
+    Private Sub picPrime_Click(sender As Object, e As EventArgs) Handles picPrime.Click
+
+    End Sub
+End Class
